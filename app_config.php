6fa55365a9ad71774246796777da360e44cac22a
Ben Platt <nohear1@hotmail.com>
2014-09-15 00:48:12 -0600

    Abstracted out variables into app_config file

---
+++ app_config.php
@@ -0,0 +1,7 @@
+<?php
+// Database connection constants
+$database_host = "localhost";
+$username = "-u root";
+$password = null;
+$database_name = "test";
+?>
--- connect.php
+++ connect.php
@@ -1,14 +1,26 @@
 <?php
-	$link = mysqli_connect("localhost",
-					"-u root")
+	require './app_config.php';
+	$link = mysqli_connect($database_host, $username)
 		or die("<p>Error Connecting to Database: " .
-			mysql_error() . "</p>");
+			mysqli_error($link) . "</p>");
 
 	echo "<p>Connected to MySQL!</p>";
 
-	mysqli_select_db($link, "test")
-		or die("<p>Error Selecting Database: test" . mysql_error() . "</p>");
+	mysqli_select_db($link, $database_name)
+		or die("<p>Error Selecting Database: test" . mysqli_error($link) . "</p>");
 
 	echo "<p>Connected to MySQL, using database test.</p>";
+
+	$result = mysqli_query($link, "SHOW TABLES;");
+	if(!$result) {
+		die("<p>Error in Listing Tables: " . mysqli_error($link) . "</p>");
+	}
+
+	echo "<p>Tables in database: </p>";
+	echo "<ul>";
+	while ($row = mysqli_fetch_row($result)) {
+		echo "<li>Table: {$row[0]}</li>";
+	}
+	echo "</ul>";
 
 ?>
